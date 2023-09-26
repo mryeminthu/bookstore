941334d912585e5e18eb7a5c4c6ad087aeb9afc1
mryeminthu <dhammaarchive@hotmail.com>
2023-09-26 09:18:40 -0700

    Deleted logo.svg etc

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Navbar from './components/Navbar';
 import Books from './pages/Books';
 import Categories from './pages/Categories';
+import store from './redux/store';
 
 const App = () => (
-  <Router>
-    <div>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Books />} />
-        <Route path="/categories" element={<Categories />} />
-      </Routes>
-    </div>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Books />} />
+          <Route path="/categories" element={<Categories />} />
+        </Routes>
+      </div>
+    </Router>
+  </Provider>
 );
 
 export default App;
